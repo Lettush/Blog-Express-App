d3961b7711d779bcc829dd1c6305daa9945615d2
Lettush <jaredneilhortaleza11@gmail.com>
2024-01-10 15:38:32 +0800

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    if (req.body) {
+        console.log("Request body:");
+        console.log(req.body);
+    }
+    next();
+});
+
+// Routes
+app.use("/api/posts", require("./src/routes/post"));
+app.use("/api/users", require("./src/routes/user"));
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen("4000", () => {
+            console.log(`Listening on port 4000 and connected to MongoDB`);
+        });
+    })
+    .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+    });
